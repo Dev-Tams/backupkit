96cb0d8d81ff4eadc5b673d196df4f6f7bd3051d
tammy alemu <tammyalemu@gmail.com>
2026-02-07 21:01:57 +0100

    implement openwriter returning io.writeCloser and path

--- internal/storage/local/local.go
+++ internal/storage/local/local.go
@@ -3,6 +3,7 @@ package local
 import (
 	"context"
 	"fmt"
+	"io"
 	"os"
 	"path/filepath"
 )
@@ -16,56 +17,46 @@ func New(name, basePath string) *Storage {
 	return &Storage{name: name, base: basePath}
 }
 
-func (s *Storage) Name() string {
-	return s.name
-}
+func (s *Storage) Name() string { return s.name }
 
-func (s *Storage) OpenWriter(_ context.Context, key string) (*Writer, error) {
+func (s *Storage) OpenWriter(_ context.Context, key string) (io.WriteCloser, string, error) {
 	finalPath := filepath.Join(s.base, filepath.FromSlash(key))
 
-	if err := os.Mkdir(filepath.Dir(finalPath), 0o755); err!= nil{
-		return nil, fmt.Errorf("mkdir: %w", err)
+	if err := os.MkdirAll(filepath.Dir(finalPath), 0o755); err != nil {
+		return nil, "", fmt.Errorf("mkdir: %w", err)
 	}
 
-	//this writes to a tmp file and rename on close
 	tmpPath := finalPath + ".tmp"
-
 	f, err := os.Create(tmpPath)
-	if err != nil{
-		return nil, fmt.Errorf("create temp: %w", err)
+	if err != nil {
+		return nil, "", fmt.Errorf("create temp: %w", err)
 	}
 
-	return &Writer{
-		f: f,
-		tmpPath: tmpPath,
-		finalPath: finalPath,
-	}, nil
+	return &Writer{f: f, tmpPath: tmpPath, finalPath: finalPath}, finalPath, nil
 }
 
-type Writer struct{
-	f *os.File
-	tmpPath string
+type Writer struct {
+	f         *os.File
+	tmpPath   string
 	finalPath string
-	closed bool
+	closed    bool
 }
 
-
-func (w *Writer) Write(p []byte)(int, error){
-	return w.f.Write(p)
-}
+func (w *Writer) Write(p []byte) (int, error) { return w.f.Write(p) }
 
 func (w *Writer) Close() error {
 	if w.closed {
 		return nil
 	}
 	w.closed = true
-	if err := w.f.Close(); err != nil{
+
+	if err := w.f.Close(); err != nil {
+		_ = os.Remove(w.tmpPath)
+		return err
+	}
+	if err := os.Rename(w.tmpPath, w.finalPath); err != nil {
 		_ = os.Remove(w.tmpPath)
 		return err
 	}
 	return nil
-}
-
-func (w *Writer) Location() string{
-	return w.finalPath
 }
