311736627abb519a1c8326919acf36e47c75c31a
tammy alemu <tammyalemu@gmail.com>
2026-02-08 19:43:34 +0100

    wire s3 into factory

--- internal/storage/factory.go
+++ internal/storage/factory.go
@@ -1,13 +1,15 @@
 package storage
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/dev-tams/backupkit/internal/config"
 	"github.com/dev-tams/backupkit/internal/storage/local"
+	s3store "github.com/dev-tams/backupkit/internal/storage/s3"
 )
 
-func FromConfig(cfg *config.Config) (map[string]Storage, error) {
+func FromConfig(ctx context.Context, cfg *config.Config) (map[string]Storage, error) {
 	out := make(map[string]Storage, len(cfg.Storage))
 
 	for _, st := range cfg.Storage {
@@ -19,8 +21,21 @@ func FromConfig(cfg *config.Config) (map[string]Storage, error) {
 			out[st.Name] = local.New(st.Name, st.Local.Path)
 
 		case "s3":
-			return nil, fmt.Errorf("storage %s: s3 not implemented yet", st.Name)
-
+			if st.S3 == nil {
+				return nil, fmt.Errorf("storage %s: s3 config missing", st.Name)
+			}
+				s, err := s3store.New(ctx, s3store.Options{
+				Name:      st.Name,
+				Bucket:    st.S3.Bucket,
+				Region:    st.S3.Region,
+				Prefix:    st.S3.Prefix,
+				AccessKey: st.S3.AccessKey,
+				SecretKey: st.S3.SecretKey,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("storage %s: %w", st.Name, err)
+			}
+			out[st.Name] = s
 		default:
 			return nil, fmt.Errorf("storage %s: unknown type %q", st.Name, st.Type)
 		}
