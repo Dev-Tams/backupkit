a4f45fb600526aba1ee4cfe42ecd4fa5e0395251
Dev-Tams <tammyalemu@gmail.com>
2026-02-17 23:00:12 +0100

    feat: add interface for local storage

---
+++ internal/storage/prunable.go
@@ -0,0 +1,18 @@
+package storage
+
+import (
+	"context"
+	"time"
+)
+
+type ObjectInfo struct {
+	Key     string
+	Size    int64
+	ModTime time.Time
+}
+
+type Prunable interface {
+	List(ctx context.Context, prefix string) ([]ObjectInfo, error)
+	Delete(ctx context.Context, key string) error
+	BasePath() string // optional helper for local storage, can be empty for other storage types
+}
